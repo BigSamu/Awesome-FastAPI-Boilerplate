05704c03e098767e23bec8152993012540af293d
Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>
2024-09-06 08:25:32 -0400

    modifying crud removing/adding methods for comment and post
    Signed-off-by: Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>

--- app/crud/post.py
+++ app/crud/post.py
@@ -10,8 +10,7 @@ from app.schemas import PostResponse, PostCreate, PostUpdate
 class CRUDPost(CRUDBase[Post, PostCreate, PostUpdate]):
     # Declare model specific CRUD operation methods.
 
-    def get_one_by_filename(self, db: Session, filename: str) -> Post:
-        return db.query(Post).filter(Post.filename == filename).first()
-
+    def get_all_from_author(self, db: Session, user_id: str) -> Post:
+        return db.query(Post).filter(Post.author.id == user_id).first()
 
 post = CRUDPost(Post)
